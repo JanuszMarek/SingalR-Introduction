a4082a71f1a460c9ffdcecb3cdb45f486be2025c
Marek <janusz.marek@corp.altegrity.com>
2020-03-28 13:34:36 +0100

    Use CORS

--- backend/SignalR-Introduction/Startup.cs
+++ backend/SignalR-Introduction/Startup.cs
@@ -25,6 +25,15 @@ namespace SignalR_Introduction
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+
             services.AddControllers();
         }
 
@@ -39,6 +48,8 @@ namespace SignalR_Introduction
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
